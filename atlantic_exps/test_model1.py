c9aae4a43a5c5b8986bf75f3667627522117e66b
nmathewa <nalex2023@my.fit.edu>
2023-10-10 18:02:17 -0400

    filter_events

--- atlantic_exps/test_model1.py
+++ atlantic_exps/test_model1.py
@@ -8,27 +8,52 @@ Created on Tue Oct 10 12:48:16 2023
 
 from keras.models import Sequential
 from keras.layers import Dense, Flatten
+from sklearn.preprocessing import StandardScaler 
 
 
-dataset = pd.read_csv(main_dir+'test_modelin1.csv')
+dataset = pd.read_csv(main_dir+'test_modelin1.csv').dropna(how='any',axis=0)
 
-features = dataset[['lead','pers','temp_mean',
-                    'pres_mean','DIST2LAND']].values
 
-target = dataset['USA_WIND'].values
 
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Dense(len(dataset), activation='relu',input_shape=(5,)),
-  tf.keras.layers.Dense(64, activation='relu'),
-  tf.keras.layers.Dense(1, activation='linear')
-])
 
-model.compile(optimizer='adam', loss='mse', metrics=['mae'])
 
-print(model.fit(features,target))
+scaler = StandardScaler()  
+
+df_Scaled = scaler.fit_transform(dataset[['lead','pers','temp_mean',
+                    'pres_mean','DIST2LAND','USA_WIND']])
+
+
+
+x_train = dataset[['pers','temp_mean',
+                    'pres_mean']]#df_Scaled[:,:-1]
+
+
+
+y_train = dataset['USA_WIND']
+
+x_train_r = x_train.values.reshape(x_train.shape[0],3)
+y_train_r = y_train.values.reshape(x_train.shape[0])
+
+#%%
+
+n_xtrain = np.array(x_train_r)
+n_ytrain = np.reshape(y_train_r, (len(y_train_r), 1))
 
 
 #%%
 
-model.fit(features,target,epochs=100)
+def set_model():
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Dense(1028,input_shape=(3,),activation='relu'))
+    #model.add(tf.keras.layers.Dense(128,activation='sigmoid'))
+    #hidden layers 
+    model.add(tf.keras.layers.Dense(512,activation='relu'))
+    model.add(tf.keras.layers.Dense(1, activation='relu'))
+    # the model is compiled with the loss function and the optimizer
+    model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+    return model
+
+
+model = set_model()
+model.fit(n_xtrain,n_ytrain,epochs=100)
 
